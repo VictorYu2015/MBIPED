5e684485e066d89987764aef7c7761babce10259
VictorYu2015 <kevin.ray.yu@gmail.com>
2020-03-08 23:04:27 +0800

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -672,11 +672,11 @@ def augment_data(base_dir,augment_both, use_all_type=True):
 
     dataset_dirs = [img_dir, gt_dir]
     # *********** starting data augmentation *********
-    #if splitting_up:
-    #    print("Image augmentation by splitting up have started!")
-    #    dataset_dirs = split_data(data_dir=dataset_dirs,augment_both=augment_both)
-    #    print("Image augmentation by splitting up have ended!")
-    splitting_up =False
+    if splitting_up:
+        print("Image augmentation by splitting up have started!")
+        dataset_dirs = split_data(data_dir=dataset_dirs,augment_both=augment_both)
+        print("Image augmentation by splitting up have ended!")
+        splitting_up =False
         
     print("123,", rotation, flipping, correction_gamma)
 
